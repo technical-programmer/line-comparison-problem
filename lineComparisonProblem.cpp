ef534f2c69ccf56e4babb4582ef7bbec0575a256
Ankit <ankitupadhyay1051@gmail.com>
2025-01-13 14:27:16 +0530

    checking is lines are equal or not ]

--- lineComparisonProblem.cpp
+++ lineComparisonProblem.cpp
@@ -5,17 +5,27 @@ using namespace std;
 int main(){
     cout << "Welcome to the Line Comparison problem" << endl;
 
-    double x1, y1, x2, y2;
+    double x1, y1, x2, y2, x3, y3, x4, y4;
 
-    cout << "Enter coordinates of the first point (x1, y1): ";
+    cout << "Enter coordinates of the first point (x1, y1) of the first line: ";
     cin >> x1 >> y1;
 
-    cout << "Enter coordinates of the second point (x2, y2): ";
+    cout << "Enter coordinates of the second point (x2, y2) of the first line: ";
     cin >> x2 >> y2;
 
-    double length = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << "Enter coordinates of the first point (x3, y3) of the second line: ";
+    cin >> x3 >> y3;
 
-    cout << "The length of the line is: " << length << endl;
+    cout << "Enter coordinates of the second point (x4, y4) of the second line: ";
+    cin >> x4 >> y4;
+
+    double length1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double length2 = sqrt(pow(x4 - x3, 2) + pow(y4 - y3, 2));
+
+    if (length1 == length2)
+        cout << "The lines are equal" << endl;
+    else
+        cout << "The lines are not equal" << endl;
 
     return 0;
 }
